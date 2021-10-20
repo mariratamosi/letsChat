eeedeb4587d209e07876a8fb5e758796430d51de
marira <rongrazzo@gmail.com>
2021-10-20 18:30:03 +0600

    test added:: function request

--- spec/support/server.spec.js
+++ spec/support/server.spec.js
@@ -1,5 +1,19 @@
+var request = require("request");
+
 describe("calculate", () => {
   it("should be 2+2", () => {
     expect(2 * 2).toBe(4);
+  });
+});
+
+describe("getMessages", () => {
+  it("it should return 200OK", (done) => {
+    request.get("http://localhost:3000/messages", (err, response) => {
+      //console.log(response);
+      expect(response.statusCode).toEqual(200);
+      expect(JSON.parse(response.body).length).toBeGreaterThan(0);
+
+      done();
+    });
   });
 });
